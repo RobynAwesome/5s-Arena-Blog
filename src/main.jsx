d20105dc4b204b69fce7b6263dc41eaa8f97ecd9
RobynAwesome <rkholofelo@gmail.com>
2026-03-18 10:20:10 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter ( [
+  {
+    Path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us<Link></Link></Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>ABout</div>,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
